1a5f6c43c7432074ac10b14e300e4521c9b0c7c3
zhouzhengyu <zhouzhengyu@super-lamps.com>
2020-12-17 11:34:39 +0800

    reorganize project,upload missing files

---
+++ algorithmTool/src/algorithmTool/arrayTools/auxiliaryTools/ArrayReorderTool.java
@@ -0,0 +1,78 @@
+package algorithmTool.arrayTools.auxiliaryTools;
+
+/**
+ * 数组重排序工具
+ * @author BlazingPhoenix
+ */
+public class ArrayReorderTool {
+	
+	/**
+	 * 随机打乱整数数组
+	 * @description 将数组打乱<br>
+	 * 				方法为将第一个元素与任一元素交换，然后对第二个元素做相同操作，以此类推<br>
+	 * 				打乱效果不理想，因为结果分布不均匀
+	 * @param array 等待被打乱的数组
+	 */
+	public static Object[] disaster(Object[] array)
+	{
+		for (int i = 0; i < array.length; i ++)
+		{
+			int x = (int)(Math.random() * array.length);
+			exchange(array, i, x);
+		}
+		return array;
+	}
+	
+	/**
+	 * 将整数数组倒转
+	 * @param array 倒转的整数数组
+	 */
+	public static Object[] reverse(Object[] array)
+	{
+		for (int i = 0; i < array.length / 2; i ++)
+			exchange(array, i, array.length - 1 - i);
+		return array;
+	}
+	
+	/**
+	 * 元素交换
+	 * @description 将一个数组的两个元素进行交换
+	 * @param array 进行修改的数组
+	 * @param index1  其中一个参与交换的元素的下标
+	 * @param index2  另一个参与交换的元素的下标
+	 */
+	public static void exchange(Object[] array, int index1, int index2)
+	{
+		Object a = array[index1];
+		array[index1] = array[index2];
+		array[index2] = a;
+	}
+	
+	/**
+	 * 插入
+	 * @description 将下标为insert的元素插入到下标为index的位置上<br>
+	 * 				下标在index和insert之间的元素相应左移或右移
+	 * @param array 数组
+	 * @param index 插入的位置的下标
+	 * @param insert 插入的元素的下标
+	 */
+	public static void insert(Object[] array, int index, int insert)
+	{
+		if (insert == index)
+			return;
+		Object flag = array[insert];
+		if (insert > index)
+		{
+			for (int i = insert; i > index; i --) {
+				array[i] = array[i - 1];
+			}
+		}
+		else
+		{
+			for (int i = insert; i < index; i ++) {
+				array[i] = array[i + 1];
+			}
+		}
+		array[index] = flag;
+	}
+}
---
+++ algorithmTool/src/algorithmTool/arrayTools/auxiliaryTools/Expander.java
@@ -0,0 +1,154 @@
+package algorithmTool.arrayTools.auxiliaryTools;
+
+/**
+ * 数组扩容器
+ * @author BlazingPhoenix
+ *
+ */
+public class Expander {
+	
+	/**
+	 * 增大到原来的两倍
+	 * @description 使用一般方法将char类型数组<br>
+	 * 				增大到原来的两倍
+	 * @param array 扩容的数组
+	 * @return 扩容后的数组
+	 */
+	public static char[] expandToDouble(char[] array)
+	{
+		char[] newArray = new char[array.length * 2];
+		for (int i = 0; i < array.length; i ++){
+			newArray[i] = array[i];
+		}
+		return newArray;
+	}
+	
+	/**
+	 * 增大到原来的两倍
+	 * @description 使用一般方法将short类型数组<br>
+	 * 				增大到原来的两倍
+	 * @param array 扩容的数组
+	 * @return 扩容后的数组
+	 */
+	public static short[] expandToDouble(short[] array)
+	{
+		short[] newArray = new short[array.length * 2];
+		for (int i = 0; i < array.length; i ++){
+			newArray[i] = array[i];
+		}
+		return newArray;
+	}
+	
+	/**
+	 * 增大到原来的两倍
+	 * @description 使用一般方法将int类型数组<br>
+	 * 				增大到原来的两倍
+	 * @param array 扩容的数组
+	 * @return 扩容后的数组
+	 */
+	public static int[] expandToDouble(int[] array)
+	{
+		int[] newArray = new int[array.length * 2];
+		for (int i = 0; i < array.length; i ++){
+			newArray[i] = array[i];
+		}
+		return newArray;
+	}
+	
+	/**
+	 * 增大到原来的两倍
+	 * @description 使用一般方法将long类型数组<br>
+	 * 				增大到原来的两倍
+	 * @param array 扩容的数组
+	 * @return 扩容后的数组
+	 */
+	public static long[] expandToDouble(long[] array)
+	{
+		long[] newArray = new long[array.length * 2];
+		for (int i = 0; i < array.length; i ++){
+			newArray[i] = array[i];
+		}
+		return newArray;
+	}
+	
+	/**
+	 * 增大到原来的两倍
+	 * @description 使用一般方法将float类型数组<br>
+	 * 				增大到原来的两倍
+	 * @param array 扩容的数组
+	 * @return 扩容后的数组
+	 */
+	public static float[] expandToDouble(float[] array)
+	{
+		float[] newArray = new float[array.length * 2];
+		for (int i = 0; i < array.length; i ++){
+			newArray[i] = array[i];
+		}
+		return newArray;
+	}
+	
+	/**
+	 * 增大到原来的两倍
+	 * @description 使用一般方法将double类型数组<br>
+	 * 				增大到原来的两倍
+	 * @param array 扩容的数组
+	 * @return 扩容后的数组
+	 */
+	public static double[] expandToDouble(double[] array)
+	{
+		double[] newArray = new double[array.length * 2];
+		for (int i = 0; i < array.length; i ++){
+			newArray[i] = array[i];
+		}
+		return newArray;
+	}
+	
+	/**
+	 * 增大到原来的两倍
+	 * @description 使用一般方法将byte类型数组<br>
+	 * 				增大到原来的两倍
+	 * @param array 扩容的数组
+	 * @return 扩容后的数组
+	 */
+	public static byte[] expandToDouble(byte[] array)
+	{
+		byte[] newArray = new byte[array.length * 2];
+		for (int i = 0; i < array.length; i ++){
+			newArray[i] = array[i];
+		}
+		return newArray;
+	}
+	
+	/**
+	 * 增大到原来的两倍
+	 * @description 使用一般方法将boolean类型数组<br>
+	 * 				增大到原来的两倍
+	 * @param array 扩容的数组
+	 * @return 扩容后的数组
+	 */
+	public static boolean[] expandToDouble(boolean[] array)
+	{
+		boolean[] newArray = new boolean[array.length * 2];
+		for (int i = 0; i < array.length; i ++){
+			newArray[i] = array[i];
+		}
+		return newArray;
+	}
+	
+	/**
+	 * 增大到原来的两倍
+	 * @description 使用一般方法将引用类型数组<br>
+	 * 				增大到原来的两倍
+	 * @param array 扩容的数组
+	 * @return 扩容后的数组
+	 */
+	public static Object[] expandToDouble(Object[] array)
+	{
+		Object[] newArray = new Object[array.length * 2];
+		for (int i = 0; i < array.length; i ++){
+			newArray[i] = array[i];
+		}
+		return newArray;
+	}
+	
+}
