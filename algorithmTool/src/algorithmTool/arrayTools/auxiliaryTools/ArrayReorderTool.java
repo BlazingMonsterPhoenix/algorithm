c8257ef5d1b62218b512b27a0101367cdfd19be1
zhouzhengyu <zhouzhengyu@super-lamps.com>
2020-12-29 18:10:19 +0800

    arrange files on 2020/12/29
    :bug: Fix bug of ArrayReorderTool.disaster

--- algorithmTool/src/algorithmTool/arrayTools/auxiliaryTools/ArrayReorderTool.java
+++ algorithmTool/src/algorithmTool/arrayTools/auxiliaryTools/ArrayReorderTool.java
@@ -7,13 +7,17 @@ package algorithmTool.arrayTools.auxiliaryTools;
 public class ArrayReorderTool {
 	
 	/**
-	 * 随机打乱整数数组
+	 * 随机打乱数组
+	 * @deprecated 逻辑存在问题<br>
+	 * 			         在元素数量为n的数组中，好的打乱算法应该使得结果存在n!种几率相同的可能性<br>
+	 * 			         并且每个元素出现在任一位置的可能性都相同且为1/n<br>
+	 * 			         而此方法的可能性为n的n次方，显然不满足条件。<br>
+	 * 			         打乱效果不理想，因为结果分布不均匀。不建议使用
 	 * @description 将数组打乱<br>
 	 * 				方法为将第一个元素与任一元素交换，然后对第二个元素做相同操作，以此类推<br>
-	 * 				打乱效果不理想，因为结果分布不均匀
 	 * @param array 等待被打乱的数组
 	 */
-	public static Object[] disaster(Object[] array)
+	public static Object[] disasterOld(Object[] array)
 	{
 		for (int i = 0; i < array.length; i ++)
 		{
@@ -21,6 +25,25 @@ public class ArrayReorderTool {
 			exchange(array, i, x);
 		}
 		return array;
+	}
+	
+	/**
+	 * 随机打乱数组
+	 * @description 将数组打乱<br>
+	 * 				先从数组中随机抽取一个元素作为新数组第一个元素，再从剩下的中抽取一个作为第二个元素，以此类推<br>
+	 * 				以抽排类比：n个人从n张牌的牌堆中抽牌。依次随机抽取，每个人拿到任一一张牌的概率相同
+	 * @param array 等待被打乱的数组
+	 * @return 打乱后的数组
+	 */
+	public static Object[] disaster(Object[] array)
+	{
+		Object[] newArray = array.clone();
+		for (int i = 0; i < newArray.length - 1; i ++)
+		{
+			int x = (int)(Math.random() * newArray.length - i) + i;
+			exchange(newArray, i, x);
+		}
+		return newArray;
 	}
 	
 	/**
